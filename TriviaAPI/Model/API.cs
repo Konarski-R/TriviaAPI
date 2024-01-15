cdc61918a1f9981f79fc513c5e56935e23dbd1e4
Konarski-R <roman.konarski@icloud.com>
2024-01-15 18:16:59 +0100

    Programm done

---
+++ TriviaAPI/Model/API.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace TriviaAPI.Model
+{
+    public class API
+    {
+        [JsonPropertyName("response_code")]
+        public int ResCode { get; set; }
+
+        [JsonPropertyName("results")]
+        public Result[] Results { get; set; }
+    }
+
+    public class Result
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("difficulty")]
+        public string Difficulty { get; set; }
+
+        [JsonPropertyName("category")]
+        public string Category { get; set; }
+
+        [JsonPropertyName("question")]
+        public string Question { get; set; }
+
+        [JsonPropertyName("correct_answer")]
+        public string CorrAnswer { get; set; }
+
+        [JsonPropertyName("incorrect_answers")]
+        public string[] IncorrAnswers { get; set; }
+
+    }
+}
---
+++ TriviaAPI/Model/Token.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace TriviaAPI.Model
+{
+	public class Token
+	{
+		[JsonPropertyName("response_code")]
+		public int ResCode { get; set; }
+
+		[JsonPropertyName("response_message")]
+		public string ResMess { get; set; }
+
+		[JsonPropertyName("token")]
+		public string Toke { get; set; }
+	}
+}
